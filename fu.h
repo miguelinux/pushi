052700e5fd612dc4b3f4e030b5d5158eeaead86a
Miguel Bernal Marin <miguel.bernal@itesm.mx>
2016-12-13 15:49:05 -0600

    start with the program

---
+++ fu.h
@@ -0,0 +1,34 @@
+#ifndef _FU_H
+#define _FU_H
+
+#include <curl/curl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * A file upload structure.
+ */
+struct _sfu {
+	char url[80]; /**< The URL to updoad the files */
+
+};
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#endif
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <fu.h>
+
+#define FILE_1 "myfile1.img"
+#define FILE_2 "myfile2.txt"
+
+
+/**
+ * The main function.
+ */
+int main (int argc, char **argv)
+{
+	return 0;
+}
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
